18efe8bb0b8ec68abceaa3f3f88bd5aa9d0d5060
Chammmppp <champ.thichanon@gmail.com>
2024-07-20 21:42:09 +0700

    Create model product

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Store
+
 
 # Register your models here.
+
+class StoreAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('product_name',)}  # To make slug auto type from product_name
+    list_display = ('product_name', 'price', 'stock', 'category', 'updated_date', 'is_available', 'is_trending')
+
+
+admin.site.register(Store)
--- store/models.py
+++ store/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from category.models import Category
+
 
 # Create your models here.
+
+class Store(models.Model):
+    product_name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField(500, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    images = models.ImageField(upload_to='photos/products')
+    stock = models.IntegerField()
+    is_available = models.BooleanField(default=True)
+    is_trending = models.BooleanField(default=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.product_name
