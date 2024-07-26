2f8f84a079f3dc1daec4567a9feeee3fab114520
Chammmppp <champ.thichanon@gmail.com>
2024-07-26 20:33:59 +0700

    Edit home template
    I've switch between price and name product

--- store/views.py
+++ store/views.py
@@ -11,12 +11,13 @@ def store(request, category_slug=None):
     products = None
 
     if category_slug is not None:
-        categories = get_object_or_404(Category, slug=category_slug)
-        products = Product.objects.filter(category=categories, is_available=True)
+        categories = get_object_or_404(Category,
+                                       slug=category_slug)  # Get an object if not exist it's will be raises 404
+        products = Product.objects.filter(category=categories,
+                                          is_available=True)  # Get object products that exist on database
     else:
         products = Product.objects.all().filter(is_available=True)
 
-    # products = Product.objects.all().filter(is_available=True)
     context = {
         'products': products,
     }
