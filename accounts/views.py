748bcab6725a595c9a06184cbe95033476a513ea
Thichanon'S <champ.thichanon@gmil.com>
2024-08-03 16:43:20 +0700

    Edit register and models views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,22 +1,27 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
+from django.contrib import messages
+from accounts.models import Account
 
 # Create your views here.
 
 
 def signup(request):
     if request.method == "POST":
-        first_name = request.POST["first_name"]
-        last_name = request.POST["last_name"]
-        email = request.POST["email"]
-        password = request.POST["password"]
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
 
-        user = User.objects.create_user(
+        if Account.objects.filter(email=email).exists():
+            messages.warning(request, "Email already exists.")
+            return render(request, "register/signup.html")
+
+        user = Account.objects.create_user(
             first_name=first_name, last_name=last_name, email=email, password=password
         )
         user.save()
+        return redirect("login")
     else:
-
         return render(request, "register/signup.html")
 
 
