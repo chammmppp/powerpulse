ad768b5149b98f787f1d348907bf56869657688c
Thichanon Sompetch <champ.thichanon@gmail.com>
2024-08-06 01:48:43 +0700

    Hidden product name that overflow

--- store/views.py
+++ store/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from store.models import Product
 from category.models import Category
-
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 # Create your views here.
 
@@ -17,11 +17,16 @@ def store(request, category_slug=None):
         products = Product.objects.filter(
             category=categories, is_available=True
         )  # Get object products that exist on database
+        product_count = products.count()
     else:
         products = Product.objects.all().filter(is_available=True)
-
+        paginator = Paginator(products, 12)
+        page = request.GET.get("page")
+        paged_products = paginator.get_page(page)
+        product_count = products.count()
     context = {
-        "products": products,
+        "products": paged_products,
+        "product_count": product_count,
     }
     return render(request, "stores/store.html", context)
 
